8e2e35e2bff4b4cd6f539ccda9b4db1593a749f9
russellkoons <rkoons@gmail.com>
2020-11-13 06:34:31 -0500

    filling out server.js

--- server.js
+++ server.js
@@ -6,3 +6,22 @@ const morgan = require('morgan');
 const passport = require('passport');
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
+
+mongoose.Promise = global.Promise;
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'Content-Type.Authorization');
+  res.header('Access-Control-Allow-Origin', 'GET,POST,PUT,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(morgan('common'));
+
