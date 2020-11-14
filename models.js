0c4d99ccdb7be0f52fa29087d3bbc2416a791380
russellkoons <rkoons@gmail.com>
2020-11-14 08:17:35 -0500

    statusSchema

--- models.js
+++ models.js
@@ -6,8 +6,37 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const statusSchema = mongoose.Schema({
-
+  user: String,
+  tasks: Array,
+  audits: Array,
+  enhancements: Array,
+  builds: Array,
+  uploads: Number,
+  tickets: Number,
+  workflows: Number,
+  reports: Number,
+  mobileUpdates: Number,
+  pageAudits: Number,
+  auditEnhancements: Number,
 });
+
+statusSchema.methods.serialize = () => {
+  return {
+    id: this._id,
+    user: this.user,
+    tasks: this.tasks,
+    audits: this.audits,
+    enhancements: this.enhancements,
+    builds: this.builds,
+    uploads: this.uploads,
+    tickets: this.tickets,
+    workflows: this.workflows,
+    reports: this.reports,
+    mobileUpdates: this.mobileUpdates,
+    pageAudits: this.pageAudits,
+    auditEnhancements: this.auditEnhancements,
+  }
+}
 
 const userSchema = mongoose.Schema({
   username: {
