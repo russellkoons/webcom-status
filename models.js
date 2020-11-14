a822c2986df6c036fc6a438223ffa0c0f680d592
russellkoons <rkoons@gmail.com>
2020-11-14 08:05:42 -0500

    added userSchema

--- models.js
+++ models.js
@@ -5,3 +5,18 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
+const statusSchema = mongoose.Schema({
+
+});
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
