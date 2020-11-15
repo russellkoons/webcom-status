a52985c25b996c7ec19585381eee421cd8b4470b
russellkoons <rkoons@gmail.com>
2020-11-15 09:57:56 -0500

    createToken function added

--- auth/router.js
+++ auth/router.js
@@ -5,7 +5,15 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET } = require('../config');
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 const router = express.Router();
+
+const createToken = user => {
+  return jwt.sign({user}, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY,
+    algorithm: 'HS256',
+  });
+}
 
 
