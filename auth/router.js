cc3ddea57d99cc89de799d6271fa0a8f7855e7fa
russellkoons <rkoons@gmail.com>
2020-11-15 10:00:38 -0500

    localAuth

--- auth/router.js
+++ auth/router.js
@@ -7,13 +7,22 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 const router = express.Router();
+router.use(bodyParser.json());
 
 const createToken = user => {
-  return jwt.sign({user}, JWT_SECRET, {
+  return jwt.sign({ user }, JWT_SECRET, {
     subject: user.username,
     expiresIn: JWT_EXPIRY,
     algorithm: 'HS256',
   });
 }
 
+const localAuth = passport.authenticate('local', { session: false });
+
+router.post('/login', localAuth, (req, res) => {
+  const authToken = createToken(req.user.serialize());
+  res.json({ authToken });
+});
+
+const jwtAuth = passport.authenticate('jwt', { session: false });
 
