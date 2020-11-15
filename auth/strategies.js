4d0a60eb8c27f7ce761e974ac659ba775ee3da8b
russellkoons <rkoons@gmail.com>
2020-11-15 10:09:12 -0500

    local search for user

--- auth/strategies.js
+++ auth/strategies.js
@@ -5,3 +5,18 @@ const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 
 const { User } = require('../models');
 const { JWT_SECRET } = require('../config');
+
+const local = new LocalStrategy((username, password, callback) => {
+  let user;
+  User.findOne({ username: username })
+    .then(_user => {
+      user = _user;
+      if (!user) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password',
+        });
+      }
+      return user.validatePassword(password);
+    })
+})
