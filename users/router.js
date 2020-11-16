c26d9dd5f6a36598c5397c2e62b7635688d0fd57
russellkoons <rkoons@gmail.com>
2020-11-16 06:12:03 -0500

    whitespace in username and password handling

--- users/router.js
+++ users/router.js
@@ -12,6 +12,15 @@ router.post('/', jsonParser, (req, res) => {
   const trimmed = ['username', 'password'];
   const notTrimmed = trimmed.find(field => req.body[field].trim() !== req.body[field]);
 
+  if (notTrimmed) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Username and Password cannot have whitespace',
+      location: notTrimmed,
+    });
+  }
+
   
 });
 
