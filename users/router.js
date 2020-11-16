704f1ca67eecb36bc24a28dac2589e423499838e
russellkoons <rkoons@gmail.com>
2020-11-16 06:07:39 -0500

    user router file without endpoints

---
+++ users/router.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { User } = require('../models');
+
+const router = express.Router();
+const jsonParser = bodyParser.json();
+
+module.exports = { router }
