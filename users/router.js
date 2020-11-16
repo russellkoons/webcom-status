f17f0fe6d335697c74d05abc2656e6d8dbf93860
russellkoons <rkoons@gmail.com>
2020-11-16 06:10:28 -0500

    started user post endpoint

--- users/router.js
+++ users/router.js
@@ -8,4 +8,11 @@ const { User } = require('../models');
 const router = express.Router();
 const jsonParser = bodyParser.json();
 
+router.post('/', jsonParser, (req, res) => {
+  const trimmed = ['username', 'password'];
+  const notTrimmed = trimmed.find(field => req.body[field].trim() !== req.body[field]);
+
+  
+});
+
 module.exports = { router }
