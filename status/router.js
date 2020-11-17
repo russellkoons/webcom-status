e41096a0d899a6e941a09fae85dc6fef9a4684e7
russellkoons <rkoons@gmail.com>
2020-11-17 15:15:35 -0500

    starter router code

---
+++ status/router.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const moment = require('moment');
+
+const { Status } = require('../models');
+
+const router = express.Router();
+
+module.exports = { router }
