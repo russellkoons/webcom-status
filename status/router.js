434960c0a126e82d3158a5afdc1cee4bff1f27a3
russellkoons <rkoons@gmail.com>
2020-11-17 15:40:19 -0500

    statusRouter DELETE endpoint

--- status/router.js
+++ status/router.js
@@ -19,4 +19,17 @@ router.get('/', (req, res) => {
     });
 });
 
+router.delete('/:id', (req, res) => {
+  Status
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      console.log(`Deleting lod ${req.params.id}`);
+      res.status(204).json({ message: 'Successful deletion' });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Deletion failed' });
+    });
+});
+
 module.exports = { router }
